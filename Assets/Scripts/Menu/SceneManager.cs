352f67aa94b00f25ab53b90dcfe7bb39758de726
Adam Feirick <afeirick96@gmail.com>
2015-09-15 17:07:47 -0400

    Added SFX to the main menu, TRC should be complete now
    Signed-off-by: Adam Feirick <afeirick96@gmail.com>

--- Assets/Scripts/Menu/SceneManager.cs
+++ Assets/Scripts/Menu/SceneManager.cs
@@ -6,11 +6,12 @@ public class SceneManager : MonoBehaviour
 {
     [SerializeField]
     Button[] buttons = new Button[7];
-
+    [SerializeField]
+    
 
     int btnIndex = 0;
     float btnTimer = 0;
-
+    float sounddelay = 2;
     // Use this for initialization
     void Start ()
     {
@@ -26,6 +27,7 @@ public class SceneManager : MonoBehaviour
             if (btnIndex > 5)
                 btnIndex = 0;
             btnTimer = 0;
+            gameObject.GetComponent<AudioSource>().Play();
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
@@ -33,12 +35,15 @@ public class SceneManager : MonoBehaviour
             if (btnIndex < 0)
                 btnIndex = 5;
             btnTimer = 0;
+            gameObject.GetComponent<AudioSource>().Play();
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            ExecuteEvents.Execute(buttons[btnIndex].gameObject, null, ExecuteEvents.submitHandler);
+
+            gameObject.GetComponent<AudioSource>().Play();
             buttons[btnIndex].image.color = Color.green;
+            ExecuteEvents.Execute(buttons[btnIndex].gameObject, null, ExecuteEvents.submitHandler);
         }
 
         GUI.FocusControl(buttons[btnIndex].name);
@@ -62,30 +67,34 @@ public class SceneManager : MonoBehaviour
 
 
 
-
-
+  
 
 
 
    public void switchToGame()
     {
+        gameObject.GetComponent<AudioSource>().Play();
         Application.LoadLevel("Level_Main");
     }
 
     public void switchToTutorial()
     {
+        gameObject.GetComponent<AudioSource>().Play();
         Application.LoadLevel("Level_Tutorial");
     }
     public void switchToCredits()
     {
+        gameObject.GetComponent<AudioSource>().Play();
         Application.LoadLevel("Menu_Credits");
     }
     public void switchToHTP()
     {
+        gameObject.GetComponent<AudioSource>().Play();
         Application.LoadLevel("Menu_HowToPlay");
     }
     public void switchToOptions()
     {
+        gameObject.GetComponent<AudioSource>().Play();
         Application.LoadLevel("Menu_Options");
     }
 
