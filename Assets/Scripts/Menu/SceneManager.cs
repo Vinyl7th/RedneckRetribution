cc5f8eb55421eae8130830dfcb6fb6d3e35bbba0
Adam Feirick <afeirick96@gmail.com>
2015-09-15 15:42:48 -0400

    Main menu functionality should be done, keyboard controls should work, buttons change colors and change scenes properly
    Signed-off-by: Adam Feirick <afeirick96@gmail.com>

--- Assets/Scripts/Menu/SceneManager.cs
+++ Assets/Scripts/Menu/SceneManager.cs
@@ -1,18 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField]
+    Button[] buttons = new Button[7];
 
-public class SceneManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    int btnIndex = 0;
+    float btnTimer = 0;
+
+    // Use this for initialization
+    void Start ()
+    {
+      
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
-	}
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            btnIndex++;
+            if (btnIndex > 5)
+                btnIndex = 0;
+            btnTimer = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            btnIndex--;
+            if (btnIndex < 0)
+                btnIndex = 5;
+            btnTimer = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ExecuteEvents.Execute(buttons[btnIndex].gameObject, null, ExecuteEvents.submitHandler);
+            buttons[btnIndex].image.color = Color.green;
+        }
+
+        GUI.FocusControl(buttons[btnIndex].name);
+        
+        for (int i = 0; i < 6; i++)
+        {
+            buttons[i].image.color = Color.white;
+        }
+        buttons[btnIndex].image.color = Color.red;
+
+        btnTimer += Time.deltaTime;
+        if (btnTimer >= 3)
+        {
+            btnIndex = 6;
+            btnTimer = 0;
+        }
+
+       
+
+    }
+
+
+
+
+
+
+
 
    public void switchToGame()
     {
