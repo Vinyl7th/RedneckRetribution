8b7075422075295262f49b93cddd8ac6ac152cac
Adam Feirick <afeirick96@gmail.com>
2015-09-15 14:48:15 -0400

    Menu now switches to each scene when the correct button is clicked, exit does nothing yet
    Signed-off-by: Adam Feirick <afeirick96@gmail.com>

---
+++ Assets/Scripts/Menu/SceneManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+   public void switchToGame()
+    {
+        Application.LoadLevel("Level_Main");
+    }
+
+    public void switchToTutorial()
+    {
+        Application.LoadLevel("Level_Tutorial");
+    }
+    public void switchToCredits()
+    {
+        Application.LoadLevel("Menu_Credits");
+    }
+    public void switchToHTP()
+    {
+        Application.LoadLevel("Menu_HowToPlay");
+    }
+    public void switchToOptions()
+    {
+        Application.LoadLevel("Menu_Options");
+    }
+
+
+}
