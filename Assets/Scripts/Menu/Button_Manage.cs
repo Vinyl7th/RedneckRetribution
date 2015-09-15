e0f42e44375f27221ceee7c2efbd5a77cac4c5d1
Adam Feirick <afeirick96@gmail.com>
2015-09-15 12:31:05 -0400

    Started working on the main menu

--- Assets/Scripts/Menu/Button_Manage.cs
+++ Assets/Scripts/Menu/Button_Manage.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Button_Manage : MonoBehaviour {
 
+	[SerializeField]
+	Button[] btns;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		btns  = new Button[6];
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		
 	}
 }
