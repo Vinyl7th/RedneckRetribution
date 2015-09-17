e7e8bb8a78936c2934da8fe3719c3ad3bf96c7a9
RyanCunningham <ryanjcunningham1@gmail.com>
2015-09-17 13:56:21 -0400

    Prepare to pull

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -73,15 +73,21 @@ public class Player : MonoBehaviour
                 }
                 else if (C_Object.tag == "Rune")
                 {
-                    C_Object.GetComponent<Rune>().current = true;
+                    if (currRune)
+                        currRune.SendMessage("ChangeCurrent");
+                    C_Object.SendMessage("ChangeCurrent");
                     currRune = C_Object;
                     standingOnObject = false;
                     C_Object = null;
-                    elementalType = currRune.GetComponent<Rune>().GetElement();
+                    //elementalType = currRune.GetComponent<Rune>().GetElement();
                 }
             }
         }
 
+    }
+    public void SetElement(int _in)
+    {
+        elementalType = _in;
     }
     void OnTriggerEnter2D(Collider2D col)
     {
---
+++ Assets/Scripts/Runes/IceRune.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceRune : MonoBehaviour {
+
+    public bool current = false;
+    int charges;
+    int element;
+    Transform thePlayer;
+    Vector3 offSet;
+    public GameObject iceAura;
+
+    // Use this for initialization
+    void Start()
+    {
+        thePlayer = GameObject.FindGameObjectWithTag("Player").transform;
+        element = 1;
+        charges = 3;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current)
+            transform.position = thePlayer.position;
+        if (charges == 0)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().RuneDestroyed();
+            Destroy(gameObject);
+        }
+    }
+    public void OnUse()
+    {
+        
+    }
+    public void ChangeCurrent()
+    {
+        if (current)
+            current = false;
+        else
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().SetElement(element);
+            current = true;
+        }
+
+    }
+}
