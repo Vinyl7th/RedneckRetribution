474e121eb1e4603d1da39c924067aed10087cc89
Charles Lewis <CDLewis@fullsail.edu>
2015-09-17 20:25:06 -0400

    Added Dynamic Game Camera

---
+++ Assets/Scripts/HUD/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public bool followPlayer = false;
+    GameObject thePlayer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        thePlayer = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (followPlayer)
+        {
+            Vector3 newCamPos = thePlayer.transform.position;
+            newCamPos.z = -10;
+            newCamPos.y += 2;
+            gameObject.transform.position = newCamPos;
+        }
+	}
+
+    public void SetPosition(Vector3 _position)
+    {
+        Vector3 newCamPos = _position;
+        newCamPos.z = -10;
+        newCamPos.y += 1.5f;
+        gameObject.transform.position = newCamPos;
+        followPlayer = false;
+    }
+
+    public void FollowPlayer()
+    {
+        followPlayer = true;
+    }
+}
---
+++ Assets/Scripts/HUD/Camera_Room_Properties.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Room_Properties : MonoBehaviour {
+
+    public bool isStatic;
+
+    GameObject theCamera;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (isStatic)
+        {
+            if (coll.gameObject.tag == "Player")
+            {
+                theCamera = GameObject.FindWithTag("MainCamera");
+                theCamera.SendMessage("SetPosition", gameObject.transform.position);
+            }
+        }
+        else
+        {
+            if (coll.gameObject.tag == "Player")
+            {
+                theCamera = GameObject.FindWithTag("MainCamera");
+                theCamera.SendMessage("FollowPlayer");
+            }
+        }
+    }
+
+    
+
+}
