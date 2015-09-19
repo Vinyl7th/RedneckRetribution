ac3c9b7896b63e896bd953ccd48ccd8380826557
Adam Feirick <afeirick96@gmail.com>
2015-09-19 14:10:56 -0400

    Passives have sprites now, they all work 100%, Added health potion and it works 100%
    Signed-off-by: Adam Feirick <afeirick96@gmail.com>

---
+++ Assets/healthPotion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class healthPotion : MonoBehaviour {
+    [SerializeField]
+    AudioSource src;
+    [SerializeField]
+    GameObject thePlayer;
+	// Use this for initialization
+	void Start ()
+    {
+        thePlayer = GameObject.FindWithTag("Player");
+        src.volume = soundController.sfxValue;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            thePlayer.GetComponent<PlayerStats>().pHealthCurr += (thePlayer.GetComponent<PlayerStats>().pHealthMax * 0.1f);
+            Destroy(gameObject);
+        }
+    }
+}
