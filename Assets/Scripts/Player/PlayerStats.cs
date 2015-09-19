87bf995e346d6b1a5f662d33cd4fde7f1221bcaf
Adam Feirick <afeirick96@gmail.com>
2015-09-19 19:30:43 -0400

    Added sfx to the player on hit
    Signed-off-by: Adam Feirick <afeirick96@gmail.com>

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -5,8 +5,8 @@ public class PlayerStats : MonoBehaviour {
 
     // Player Stats
     
-    public float pHealthMax = 0.0f;
-    public float pHealthCurr = 0.0f;
+    public float pHealthMax = 1000.0f;
+    public float pHealthCurr = 1000.0f;
     public float pAttack = 0.0f;
     public float pDefense = 0.0f;
     public float pAttackSpeed = 0.0f;
@@ -14,13 +14,15 @@ public class PlayerStats : MonoBehaviour {
     public float pLifeSteal = 0.0f;
 
     public bool pIsAlive = true;
-
+    AudioSource src;
 
 
 	// Use this for initialization
 	void Start ()
     {
         pDefense = 0;
+        src = gameObject.GetComponent<AudioSource>();
+        src.volume = soundController.sfxValue;
 	
 	}
 	
@@ -31,9 +33,11 @@ public class PlayerStats : MonoBehaviour {
 
     public void TakeDamage(float _damage)
     {
+        src.Play();
         pHealthCurr -= _damage;
         if (pHealthCurr <= 0.0f)
         {
+            
             pHealthCurr = 0.0f;
             pIsAlive = false;
         }
