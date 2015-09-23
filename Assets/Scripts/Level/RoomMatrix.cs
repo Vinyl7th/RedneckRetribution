918484fccc6562e0752549fa68ee17810adcee1b
Charles Lewis <CDLewis@fullsail.edu>
2015-09-22 20:13:40 -0400

    Fixing idfk anymore

--- Assets/Scripts/Level/RoomMatrix.cs
+++ Assets/Scripts/Level/RoomMatrix.cs
@@ -6,8 +6,26 @@ public class RoomMatrix : MonoBehaviour {
     public int Row;
     public int Coll;
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    GameObject[] Combat_UP_RIGHT;
+
+    [SerializeField]
+    GameObject[] Combat_UP_DOWN;
+
+    [SerializeField]
+    GameObject[] Combat_UP_LEFT;
+
+    [SerializeField]
+    GameObject[] Combat_RIGHT_DOWN;
+
+    [SerializeField]
+    GameObject[] Combat_RIGHT_LEFT;
+
+    [SerializeField]
+    GameObject[] Combat_DOWN_LEFT;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
