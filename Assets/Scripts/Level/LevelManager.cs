0d6048b10e6fa6c3949ad4f7c879a0b126b262d5
Charles Lewis <CDLewis@fullsail.edu>
2015-09-25 21:11:45 -0400

    Level stuff

--- Assets/Scripts/Level/LevelManager.cs
+++ Assets/Scripts/Level/LevelManager.cs
@@ -3,13 +3,65 @@ using System.Collections;
 
 public class LevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public GameObject Floor1;
+    public GameObject Floor2;
+    public GameObject Floor3;
+    public GameObject Floor4;
+    public GameObject Floor5;
+
+    // Use this for initialization
+    void Start ()
+    {
+        LoadLevel(1);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+    public void LoadLevel(int _level)
+    {
+        GameObject[] levelDat = GameObject.FindGameObjectsWithTag("ROOM");
+        for (int i = 0; i < levelDat.Length; i++)
+        {
+            Destroy(levelDat[i]);
+        }
+
+        GameObject thePlayer = GameObject.FindWithTag("Player");
+        Vector3 spawn = new Vector3(0, 0, 0);
+        thePlayer.transform.position = spawn;
+
+
+        switch (_level)
+        {
+            case 1:
+                {
+                    Instantiate(Floor1, spawn, gameObject.transform.rotation);
+                    break;
+                }
+            case 2:
+                {
+                    Instantiate(Floor2, spawn, gameObject.transform.rotation);
+                    break;
+                }
+            case 3:
+                {
+                    Instantiate(Floor3, spawn, gameObject.transform.rotation);
+                    break;
+                }
+            case 4:
+                {
+                    Instantiate(Floor4, spawn, gameObject.transform.rotation);
+                    break;
+                }
+            case 5:
+                {
+                    Instantiate(Floor5, spawn, gameObject.transform.rotation);
+                    break;
+                }
+
+        }
+    }
 }
