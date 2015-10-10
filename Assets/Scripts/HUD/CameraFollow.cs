79d9b7465b57c6851cb857b392ec4dc1c0496103
Christopher Conklin <aa>
2015-10-09 23:59:47 -0400

    awdawdwa

--- Assets/Scripts/HUD/CameraFollow.cs
+++ Assets/Scripts/HUD/CameraFollow.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
+    bool loading;
+    float delay;
+    Vector3 loadingscreen;
+
     public bool followPlayer = false;
     GameObject thePlayer;
     [SerializeField]
@@ -10,6 +14,10 @@ public class CameraFollow : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        
+        loading = true;
+        loadingscreen = new Vector3(-93.71f, 131.9f, -10);
+        transform.position = loadingscreen;
         thePlayer = GameObject.FindWithTag("Player");
         src.volume = soundController.musicValue;
 	}
@@ -17,23 +25,48 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (followPlayer)
+        if (!loading)
         {
-            Vector3 newCamPos = thePlayer.transform.position;
-            newCamPos.z = -10;
-            newCamPos.y += 2;
-            gameObject.transform.position = newCamPos;
+            if (followPlayer)
+            {
+                Vector3 newCamPos = thePlayer.transform.position;
+                newCamPos.z = -10;
+                newCamPos.y += 2;
+                gameObject.transform.position = newCamPos;
+            }
         }
-	}
+        else
+        {
+            delay += Time.deltaTime;
+            transform.position = loadingscreen;
+            if (delay >= 1)
+            {
+                loading = false;
+            }
+        }
+    }
 
     public void SetPosition(Vector3 _position)
     {
-        Vector3 newCamPos = _position;
-        newCamPos.z = -10;
-        newCamPos.y += 1.5f;
-        gameObject.transform.position = newCamPos;
-        followPlayer = false;
-    }
+        if (!loading)
+        {
+            Vector3 newCamPos = _position;
+            newCamPos.z = -10;
+            newCamPos.y += 1.5f;
+            gameObject.transform.position = newCamPos;
+            followPlayer = false;
+        }
+        else
+        {
+            delay += Time.deltaTime;
+            transform.position = loadingscreen;
+            if (delay >= 1)
+            {
+                loading = false;
+            }
+        }
+     }
+    
 
     public void FollowPlayer()
     {
