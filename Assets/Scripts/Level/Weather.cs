da29a970b825ea0ee4326403aa30409f965cf5ec
RyanCunningham <ryanjcunningham1@gmail.com>
2015-10-12 17:49:30 -0400

    changes

--- Assets/Scripts/Level/Weather.cs
+++ Assets/Scripts/Level/Weather.cs
@@ -3,32 +3,59 @@ using System.Collections;
 
 public class Weather : MonoBehaviour
 {
-  
+
     [SerializeField]
     GameObject rain;
+    [SerializeField]
+    GameObject snow;
+    int effect;
     float offsetX;
     float offsetY;
     Vector3 position;
     // Use this for initialization
     void Start()
     {
-      
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i <3; i++)
+        effect = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraFollow>().weatherType;
+        if (effect == 0)
         {
 
-        position = Camera.main.transform.position;
-        position.z = 1;
-        offsetX = Random.Range(-16.0f, 16.0f);
-        offsetY = Random.Range(-10.0f, 10.0f);
-        position.x += offsetX;
-        position.y += offsetY;
-        Instantiate(rain, position, rain.transform.rotation);
-   }
         }
+        else if (effect == 1)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+
+                position = Camera.main.transform.position;
+                position.z = 1;
+                offsetX = Random.Range(-16.0f, 16.0f);
+                offsetY = Random.Range(-10.0f, 10.0f);
+                position.x += offsetX;
+                position.y += offsetY;
+                Instantiate(rain, position, rain.transform.rotation);
+            }
+        }
+        else if (effect == 2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+
+                position = Camera.main.transform.position;
+                position.z = 1;
+                offsetX = Random.Range(-16.0f, 16.0f);
+                offsetY = Random.Range(-10.0f, 10.0f);
+                position.x += offsetX;
+                position.y += offsetY;
+                Instantiate(snow, position, rain.transform.rotation);
+            }
+        }
+    }
 }
+
